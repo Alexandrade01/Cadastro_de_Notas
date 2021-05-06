59ffb091eb733d231b379b3c4c6607fb90e1b977
alexa <alexa@DESKTOP-2QVCB30>
2021-05-05 23:35:54 -0300

    Version 1.0

---
+++ AlunoHeranca/AlunoBase.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlunoHeranca
+{
+    class AlunoBase : Object
+    {
+        public int RA { get; set; }
+        public string Nome { get; set; }
+        public double N1 { get; set; }
+        public int Faltas { get; set; }
+
+        public Aprovacao situacao { get; set; }
+
+        public virtual double CalcularMedia() // virtual significa que um método *pode* ser modificado nas classes descendentes
+        {
+            return N1;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("RA: " + RA);
+            sb.AppendLine("Nome: " + Nome);
+            sb.AppendLine("N1: " + N1.ToString("0.0"));
+            sb.AppendLine("Média: " + CalcularMedia().ToString("0.0"));
+            sb.AppendLine("Frequencia do Aluno: %" + FrequenciaAluno().ToString("0.0"));
+            sb.AppendLine("Situação :" + situacao);
+
+            return sb.ToString();
+        }
+        public double FrequenciaAluno()
+        {
+            return ((double)(80-Faltas)/80) * 100;
+        }
+
+        public void Avaliacao(int corte)
+        {
+            if (CalcularMedia() >= corte && FrequenciaAluno() >= 75)
+                situacao = Aprovacao.Aprovado;
+            else
+                situacao = Aprovacao.Reprovado;
+           
+        }
+
+        
+    }
+}
---
+++ AlunoHeranca/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlunoHeranca
+{
+    public partial class Form1 : Form
+    {
+        List<AlunoBase> alunos = new List<AlunoBase>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void RequisitosBasicos(AlunoBase b)
+        {
+            b.RA = Convert.ToInt32(txtRA.Text);
+            b.Nome = txtNomeAluno.Text;
+            b.N1 = Convert.ToDouble(txtN1.Text);
+            b.Faltas = Convert.ToInt32(txtFaltas.Text);
+           
+        }
+
+        private void BtnAlunoEM(object sender, EventArgs e)
+        {
+            try
+            {
+                AlunoEM a = new AlunoEM();
+                RequisitosBasicos(a);
+                a.N2 = Convert.ToDouble(txtN2_EM.Text);
+                a.N3 = Convert.ToDouble(txtN3_EM.Text);
+                a.Responsavel = txtResponsavel.Text;
+                a.Avaliacao(6);
+
+
+                alunos.Add(a);
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show(erro.Message);
+            }
+
+        }
+
+        private void BtnAlunoFtt(object sender, EventArgs e)
+        {
+            try
+            {
+                AlunoFTT a = new AlunoFTT();
+                RequisitosBasicos(a);
+                a.N2 = Convert.ToDouble(txtN2_FTT.Text);
+                a.HorasEstagio = Convert.ToInt32(txtHorasEstagio.Text);
+                a.Avaliacao(5);
+
+                alunos.Add(a);
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show(erro.Message);
+            }
+        }
+
+        private void BtnAlunoPos(object sender, EventArgs e)
+        {
+            try
+            {
+                var a = new AlunoPos();
+                RequisitosBasicos(a);
+                a.InstituicaoEnsinoSuperior = txtGraduacao.Text;
+                a.Avaliacao(7);
+
+                alunos.Add(a);
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show(erro.Message);
+            }
+        }
+
+        private void BtnListagem(object sender, EventArgs e)
+        {
+            txtListagem.Clear();
+            StringBuilder sb = new StringBuilder();
+
+            foreach(AlunoBase aluno in alunos)
+            {
+                sb.AppendLine("RA: " + aluno.RA);
+                sb.AppendLine("Nome: " + aluno.Nome);
+                sb.AppendLine("N1: " + aluno.N1.ToString("0.0"));
+                sb.AppendLine("Frequencia do Aluno: %" + aluno.FrequenciaAluno());
+
+                if (aluno is AlunoEM)
+                {
+                    sb.AppendLine("N2: " + (aluno as AlunoEM).N2.ToString("0.0"));
+                    sb.AppendLine("N3: " + (aluno as AlunoEM).N3.ToString("0.0"));
+                    sb.AppendLine("Responsável: " + (aluno as AlunoEM).Responsavel);
+                }
+                else if (aluno is AlunoFTT)
+                {
+                    sb.AppendLine("N2: " + (aluno as AlunoFTT).N2.ToString("0.0"));
+                    sb.AppendLine("Horas de estágio: " + (aluno as AlunoFTT).HorasEstagio);
+                }
+                else
+                {
+                    sb.AppendLine("Graduado em: " + ((AlunoPos)aluno).InstituicaoEnsinoSuperior);
+                }
+
+                sb.AppendLine("Média: " + aluno.CalcularMedia().ToString("0.0"));
+                sb.AppendLine("");
+                sb.AppendLine("");
+            }
+
+            txtListagem.Text = sb.ToString();
+        }
+
+        private void ListagemToString(object sender, EventArgs e)
+        {
+            txtListagem.Clear();
+            StringBuilder sb = new StringBuilder();
+            foreach (AlunoBase aluno in alunos)
+            {
+                sb.AppendLine(aluno.ToString());
+                sb.AppendLine("");
+                sb.AppendLine("");
+            }
+            txtListagem.Text = sb.ToString();
+        }
+
+        private void BtnListaEM_Click(object sender, EventArgs e)
+        {
+            txtListagem.Clear();
+            StringBuilder sb = new StringBuilder();
+            foreach(AlunoBase aluno in alunos)
+            {
+                if(aluno is AlunoEM)
+                {
+                    sb.AppendLine(aluno.ToString());
+                    sb.AppendLine("");
+                    sb.AppendLine("");
+                   
+                }
+                
+
+            }
+            txtListagem.Text = sb.ToString();
+        }
+
+        private void BtnListaFTT_Click(object sender, EventArgs e)
+        {
+            txtListagem.Clear();
+            StringBuilder sb = new StringBuilder();
+            foreach (AlunoBase aluno in alunos)
+            {
+                if (aluno is AlunoFTT)
+                {
+                    sb.AppendLine(aluno.ToString());
+                    sb.AppendLine("");
+                    sb.AppendLine("");
+
+                }
+                
+
+            }
+            txtListagem.Text = sb.ToString();
+
+        }
+
+        private void BtnListaPos_Click(object sender, EventArgs e)
+        {
+            txtListagem.Clear();
+            StringBuilder sb = new StringBuilder();
+            foreach (AlunoBase aluno in alunos)
+            {
+                if (aluno is AlunoPos)
+                {
+                    sb.AppendLine(aluno.ToString());
+                    sb.AppendLine("");
+                    sb.AppendLine("");
+
+                }
+               
+
+            }
+            txtListagem.Text = sb.ToString();
+        }
+    }
+}
